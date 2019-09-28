f65dff98a80c9ec4acc3b3e76175020d560ae6ad
Nelly <nkhmeleva@yahoo.com>
2019-09-28 11:30:39 -0600

    added create action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,18 +8,26 @@ class MoviesController < ApplicationController
     end 
 
 
-    # def create
-    #     @movie = Movie.new
-    #     render json: @movies.to_json(:include => {
-    #         :likes => {:only => [:number_of_likes]},
-    #     })
+    def create
+        @movie = Movie.create(movies_params)
+        render json: @movie.to_json(:include => {
+            :likes => {:only => [:number_of_likes]},
+        })
     
-    #     @movie.save
+        
 
-    # end
+    end
 
 
+    # def create
+    #    @movie = Movie.new(movies_params)
+    # end 
 
        
+    private 
+
+    def movies_params
+        params.require(:movie).permit(:image, :title, :sum, :release_year)
+    end 
 
 end
