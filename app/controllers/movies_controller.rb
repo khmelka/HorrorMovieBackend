8183e0981755fa9489d8fcd87f9ee7346e21a6d8
Nelly <nkhmeleva@yahoo.com>
2019-11-07 15:41:12 -0700

    added delete action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+    # before_action :set_movie, only: [:show, :destroy]
     
     def index 
         @movies = Movie.all
@@ -7,24 +8,28 @@ class MoviesController < ApplicationController
         })
     end 
 
+    def show
+        render json: @movie
+    end 
+
 
     def create
         @movie = Movie.create(movies_params)
         render json: @movie.to_json(:include => {
             :likes => {:only => [:number_of_likes]},
         })
-    
-        
-
     end
 
-
-    # def create
-    #    @movie = Movie.new(movies_params)
-    # end 
-
+    def destroy
+        @movie = Movie.find(params[:id])
+        @movie.destroy
+    end 
        
     private 
+
+    # def set_movie
+    #     @movie = Movie.find(params[:id])
+    # end 
 
     def movies_params
         params.require(:movie).permit(:image, :title, :sum, :release_year)
